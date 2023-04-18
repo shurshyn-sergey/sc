ec2a7862a7a88c4c503f15dbd682ad6623bf748c
shurshyn-sergey <Sergey.Shurshyn@gmail.com>
2023-04-18 17:18:42 +0300

    install fixes

--- sc/install.sh
+++ sc/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-curr_dir=$(pwd)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-find ./bin -type f -exec chmod u+x {} \;
+find "$SCRIPT_DIR"/bin -type f -exec chmod u+x {} \;
 
 SC='/usr/local/sc'
 mkdir $SC
 
-cp -R "$curr_dir"/* "$SC"
+cp -R "$SCRIPT_DIR"/* "$SC"
 
 # Configuring system env
 echo "export SC='$SC'" > /etc/profile.d/sc.sh
@@ -18,24 +18,24 @@ echo 'export PATH' >> /root/.bash_profile
 source /root/.bash_profile
 
 
-apt install software-properties-common
-add-apt-repository ppa:ondrej/php
+apt -y install software-properties-common
+add-apt-repository -y ppa:ondrej/php
 apt update
 
-apt install php7.4 php7.4-fpm
+apt install -y php7.4 php7.4-fpm
 apt install -y php7.4-cli php7.4-common php7.4-json php7.4-curl php7.4-dom php7.4-intl php7.4-mbstring php7.4-mysqli php7.4-mysqlnd php7.4-SimpleXML php7.4-xml php7.4-xmlreader php7.4-xmlwriter php7.4-xsl php7.4-zip php7.4-gd php7.4-bcmath
 
-apt install php8.1 php8.1-fpm
+apt install -y php8.1 php8.1-fpm
 apt install -y php8.1-cli php8.1-common  php8.1-curl php8.1-dom php8.1-intl php8.1-mbstring php8.1-mysqli php8.1-mysqlnd php8.1-SimpleXML php8.1-xml php8.1-xmlreader php8.1-xmlwriter php8.1-xsl php8.1-zip php8.1-gd php8.1-bcmath
 
-apt install php8.2 php8.2-fpm
+apt install -y php8.2 php8.2-fpm
 apt install -y php8.2-cli php8.2-common  php8.2-curl php8.2-dom php8.2-intl php8.2-mbstring php8.2-mysqli php8.2-mysqlnd php8.2-SimpleXML php8.2-xml php8.2-xmlreader php8.2-xmlwriter php8.2-xsl php8.2-zip php8.2-gd php8.2-bcmath
 
-apt install redis-server
+apt install -y redis-server
 
 ./bin/sc-redis-config
 
-apt install mysql-server
+apt install -y mysql-server
 systemctl start mysql.service
 
 apt install -y nginx
